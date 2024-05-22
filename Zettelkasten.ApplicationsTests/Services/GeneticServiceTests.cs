1d00970ca081e998ea3b3d4b528b9feede8b2ba4
Ondrya <ondrya@yandex.ru>
2024-05-22 05:57:48 +0300

    add test

---
+++ Zettelkasten.ApplicationsTests/Services/GeneticServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zettelkasten.Applications.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zettelkasten.Applications.Services.Tests
+{
+    [TestClass()]
+    public class GeneticServiceTests
+    {
+        [TestMethod()]
+        public void GeneticServiceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MutateCollectionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreatePopulationFirstTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CheckCollectionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SelectionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FilterPopulationTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
